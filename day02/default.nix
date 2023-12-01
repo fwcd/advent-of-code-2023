ea60415e8ad0cbd794d8b4497a9d1ea89f793af4
fwcd <fwcdmail@gmail.com>
2023-12-01 12:49:49 +0000

    Add day 2 stub in COBOL

---
+++ day02/default.nix
@@ -0,0 +1,20 @@
+{ pkgs ? import <nixpkgs> {} }:
+  pkgs.stdenv.mkDerivation {
+    name = "advent-of-code-2023-day02";
+    src = ./src;
+
+    buildInputs = [
+      (pkgs.gnu-cobol.overrideAttrs (_: { doInstallCheck = false; })).bin
+    ];
+
+    buildPhase = ''
+      mkdir -p out
+      cobc -xo out/day02 day02.cob
+      ls out
+    '';
+
+    installPhase = ''
+      mkdir -p $out/bin
+      cp out/day02 $out/bin
+    '';
+  }
---
+++ day02/src/day02.cob
@@ -0,0 +1,7 @@
+      * We have to indent by 7 spaces, because... because!
+
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. DAY02.
+       PROCEDURE DIVISION.
+       DISPLAY "Hello World!".
+       STOP RUN.
