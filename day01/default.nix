4cf68a7de8c4a76c5f2598d38421027492419088
fwcd <fwcdmail@gmail.com>
2023-12-01 02:16:05 +0000

    Set up basic day 1 project in C

---
+++ day01/default.nix
@@ -0,0 +1,15 @@
+{ pkgs ? import <nixpkgs> {} }:
+  pkgs.stdenv.mkDerivation {
+    name = "advent-of-code-2023-day01";
+    src = ./src;
+
+    buildPhase = ''
+      mkdir -p out
+      cc -o out/day01 day01.c
+    '';
+
+    installPhase = ''
+      mkdir -p $out/bin
+      cp out/day01 $out/bin
+    '';
+  }
