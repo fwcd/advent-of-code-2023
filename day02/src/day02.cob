3638bd5b02665a0744f3d86a19b144961baaec17
fwcd <fwcdmail@gmail.com>
2023-12-01 14:33:35 +0000

    Read CLI argument in COBOL

--- day02/src/day02.cob
+++ day02/src/day02.cob
@@ -2,6 +2,18 @@
 
        IDENTIFICATION DIVISION.
        PROGRAM-ID. DAY02.
+
+       DATA DIVISION.
+           WORKING-STORAGE SECTION.
+               01 FileName PIC X(100).
+
        PROCEDURE DIVISION.
-       DISPLAY "Hello World!".
+           ACCEPT FileName FROM COMMAND-LINE.
+
+           IF FileName = SPACES THEN
+               DISPLAY "Usage: day02 <path to input>"
+               STOP RUN
+           END-IF.
+
+           DISPLAY FileName.
        STOP RUN.
