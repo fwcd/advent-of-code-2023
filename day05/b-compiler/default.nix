60a9fbcf7fc9da5fe0b09cc0d1920a6c0c7c2d2b
fwcd <fwcdmail@gmail.com>
2023-12-04 22:46:00 +0000

    Package the B compiler

---
+++ day05/b-compiler/default.nix
@@ -0,0 +1,36 @@
+# This is the B compiler we are using: https://cpjsmith.uk/b
+# It compiles B programs to a bytecode that closely mirrors Ken Thompson's
+# reference machine and ships an interpreter for this bytecode.
+{ pkgs ? import <nixpkgs> {} }:
+  pkgs.stdenv.mkDerivation rec {
+    name = "b-compiler";
+    version = "1.1";
+    src = pkgs.fetchurl {
+      url = "https://cpjsmith.uk/downloads/b/b-${version}.tar.gz";
+      sha256 = "bd1ff5ca12dbfa82ffe966ac248f9c939f4ca5493734f6dc836679aa49a9cb1a";
+    };
+
+    patches = [
+      patches/fix-cerr-endl-typo.patch
+      patches/std-cxx-03.patch
+    ];
+
+    # The tarball does not contain a 'top-level directory', therefore we'll have to do this
+    sourceRoot = ".";
+
+    buildPhase = ''
+      make CXX=$CXX
+    '';
+
+    installPhase = ''
+      mkdir -p $out/{bin,share/b}
+
+      cp {bc,ba,br} $out/bin
+      cp libb.ba $out/share/b
+    '';
+
+    meta = {
+      description = "A bytecode compiler for the B programming language";
+      homepage = "https://cpjsmith.uk/b";
+    };
+  }
