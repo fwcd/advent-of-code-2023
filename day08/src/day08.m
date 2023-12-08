1e5054ed1094e7f78ba01af157488f3d4df59767
fwcd <fwcdmail@gmail.com>
2023-12-08 16:56:32 +0000

    Require command line argument for day 8

--- day08/src/day08.m
+++ day08/src/day08.m
@@ -1,6 +1,10 @@
 #import <Foundation/Foundation.h>
 
 int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    fprintf(stderr, "Usage: %s <input>\n", argv[0]);
+    return 1;
+  }
   NSLog(@"Hello world!");
   return 0;
 }
