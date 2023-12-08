447f0747633fc2e003f9cc6b7b0a9331c27b972c
fwcd <fwcdmail@gmail.com>
2023-12-08 10:39:51 +0000

    Use DCG to parse day 7 input

--- day07/src/day07.pl
+++ day07/src/day07.pl
@@ -1,8 +1,30 @@
 :- use_module(library(dcg/basics)).
 
+% +---------------------------+
+% | DCG for parsing the input |
+% +---------------------------+
+
+dcg_hands([])           --> eos, !.
+dcg_hands([Hand|Hands]) --> dcg_hand(Hand), dcg_hands(Hands).
+
+dcg_hand(hand(Cards, Bid)) --> dcg_cards(Cards), " ", dcg_bid(Bid), eol.
+
+dcg_cards(Cards) --> string(Cards).
+
+dcg_bid(Bid) --> number(Bid).
+
+% +--------------+
+% | Main program |
+% +--------------+
+
+read_input(Path, Input) :-
+  phrase_from_file(dcg_hands(Input), Path).
+
 main :-
   current_prolog_flag(argv, [Cmd|Args]),
   (
     Args = [] -> write('Usage: '), write(Cmd), writeln(' <path to input>');
-    Args = [InputPath|_] -> writeln(InputPath) % TODO: Implement it!
+    Args = [InputPath|_] ->
+      read_input(InputPath, Input),
+      writeln(Input)
   ).
