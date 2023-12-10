fac8de876e235a8589b34fa1a01d926cb244e386
fwcd <fwcdmail@gmail.com>
2023-12-10 22:12:45 +0000

    Try tracking inner candidates

--- day10/src/Day10.java
+++ day10/src/Day10.java
@@ -2,12 +2,15 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Deque;
 import java.util.HashSet;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Day10 {
@@ -28,8 +31,12 @@ public class Day10 {
   }
 
   public static record Maze(List<String> lines) {
+    public int getHeight() { return lines.size(); }
+
+    public int getWidth() { return lines.get(0).length(); }
+
     public boolean isInBounds(Vec2 pos) {
-      return pos.x >= 0 && pos.x < lines.get(0).length() && pos.y >= 0 && pos.y < lines.size();
+      return pos.x >= 0 && pos.x < getWidth() && pos.y >= 0 && pos.y < getHeight();
     }
 
     public char get(Vec2 pos) {
@@ -49,17 +56,10 @@ public class Day10 {
       };
     }
 
-    public Stream<Vec2> getInnerNeighbors(Vec2 pos) {
-      return switch (get(pos)) {
-        case '.' -> pos.cardinalNeighbors().filter(n -> get(n) == '.');
-        default -> Stream.empty();
-      };
-    }
-
     public Optional<Vec2> locate(char value) {
-      for (int y = 0; y < lines.size(); y++) {
+      for (int y = 0; y < getHeight(); y++) {
         String line = lines.get(y);
-        for (int x = 0; x < line.length(); x++) {
+        for (int x = 0; x < getWidth(); x++) {
           if (line.charAt(x) == value) {
             return Optional.of(new Vec2(x, y));
           }
@@ -72,13 +72,13 @@ public class Day10 {
 
     private static record BfsNode(Vec2 position, Vec2 tangent, int tangentSign, int distance, BfsNodeType type) {}
 
-    private static record BfsResult(Set<Vec2> visited, int maxDistance, int innerCount) {}
+    public static record BfsResult(Set<Vec2> visited, int maxDistance, Set<Vec2> inner) {}
 
     public BfsResult bfs(Vec2 start) {
       Deque<BfsNode> queue = new ArrayDeque<>(List.of(new BfsNode(start, new Vec2(0, 0), 1, 0, BfsNodeType.START)));
       Set<Vec2> visited = new HashSet<>();
+      Set<Vec2> innerCandidates = new HashSet<>();
       int maxDistance = 0;
-      int innerCount = 0;
 
       while (!queue.isEmpty()) {
         BfsNode node = queue.poll();
@@ -88,7 +88,7 @@ public class Day10 {
 
           switch (node.type) {
           case BfsNodeType.INNER:
-            innerCount++;
+            innerCandidates.add(node.position);
             break;
           case BfsNodeType.PIPE:
             Vec2 inwardNormal = node.tangent.rotate90();
@@ -104,24 +104,44 @@ public class Day10 {
           int i = 0;
           Stream<Vec2> neighbors = (
             node.type == BfsNodeType.INNER
-              ? getInnerNeighbors(node.position)
+              ? node.position.cardinalNeighbors()
               : getPipeNeighbors(node.position)
           ).filter(this::isInBounds);
 
           for (Vec2 neighbor : (Iterable<Vec2>) neighbors::iterator) {
             int tangentSign = node.tangentSign;
-            if (node.type == BfsNodeType.START && i % 2 == 0) {
+            if (node.type == BfsNodeType.START && i % 2 == 1) {
               tangentSign *= -1;
             }
             Vec2 tangent = neighbor.minus(node.position).scale(tangentSign);
             BfsNodeType type = node.type == BfsNodeType.START ? BfsNodeType.PIPE : node.type;
+            if (node.type == BfsNodeType.PIPE && innerCandidates.contains(neighbor)) {
+              innerCandidates.remove(neighbor);
+              visited.remove(neighbor);
+            }
             queue.offer(new BfsNode(neighbor, tangent, tangentSign, node.distance + 1, type));
             i++;
           }
         }
       }
 
-      return new BfsResult(visited, maxDistance, innerCount);
+      System.out.println(toString(innerCandidates));
+
+      return new BfsResult(visited, maxDistance, innerCandidates);
+    }
+
+    public String toString(Set<Vec2> markedPositions) {
+      return IntStream.range(0, getHeight())
+        .mapToObj(y -> IntStream.range(0, getWidth())
+          .mapToObj(x -> new Vec2(x, y))
+          .map(pos -> markedPositions.contains(pos) ? "\033[31;1;4mX\033[0m" : Character.toString(get(pos)))
+          .collect(Collectors.joining()))
+        .collect(Collectors.joining("\n"));
+    }
+
+    @Override
+    public String toString() {
+      return toString(Set.of());
     }
   }
 
@@ -135,6 +155,6 @@ public class Day10 {
     var bfsResult = maze.bfs(maze.locate('S').orElseThrow(() -> new NoSuchElementException("No start found")));
 
     System.out.println("Part 1: " + bfsResult.maxDistance);
-    System.out.println("Part 2: " + bfsResult.innerCount);
+    System.out.println("Part 2: " + bfsResult.inner.size());
   }
 }
