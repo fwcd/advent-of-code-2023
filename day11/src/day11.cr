e8fc9b0b7249e18b938ceb3df08663b39e2b4b65
fwcd <fwcdmail@gmail.com>
2023-12-11 20:00:30 +0000

    Implement day 11 part 2

--- day11/src/day11.cr
+++ day11/src/day11.cr
@@ -3,16 +3,16 @@ unless ARGV.size >= 1
   exit 1
 end
 
-alias Position = Array(Int32)
+alias Position = Array(Int64)
 
-def expand_1d(positions : Array(Position), axis : Int32) : Array(Position)
+def expand_1d(positions : Array(Position), axis : Int64, factor : Int64) : Array(Position)
   sorted = positions.sort_by { |pos| pos[axis] }
   sorted.reduce({0, [] of Position}) do |acc, pos|
     offset, acc_positions = acc
     unless acc_positions.empty?
       delta = pos[axis] + offset - acc_positions.last[axis]
       if delta > 1
-        offset += delta - 1
+        offset += (delta - 1) * (factor - 1)
       end
     end
     if offset > 0
@@ -23,25 +23,25 @@ def expand_1d(positions : Array(Position), axis : Int32) : Array(Position)
   end[1]
 end
 
-def expand_2d(positions : Array(Position)) : Array(Position)
-  expand_1d(expand_1d(positions, 1), 0)
+def expand_2d(positions : Array(Position), factor : Int64) : Array(Position)
+  expand_1d(expand_1d(positions, 1, factor), 0, factor)
 end
 
 def find_galaxies(lines : Array(String)) : Array(Position)
   lines.map_with_index do |line, y|
     line.chars.map_with_index do |c, x|
       if c == '#'
-        [x, y]
+        [x.to_i64, y.to_i64]
       end
     end.compact
   end.flat_map &.itself
 end
 
-def manhattan(pos1 : Position, pos2 : Position) : Int32
+def manhattan(pos1 : Position, pos2 : Position) : Int64
   pos1.zip(pos2).map { |ps| (ps[1] - ps[0]).abs }.sum
 end
 
-def solve(positions : Array(Position)) : Int32
+def solve(positions : Array(Position)) : Int64
   positions.map_with_index do |pos1, i|
     positions[(i + 1)..].map_with_index do |pos2, j|
       manhattan(pos1, pos2)
@@ -53,7 +53,9 @@ path = ARGV[0]
 lines = File.read_lines(path)
 
 positions = find_galaxies(lines)
-positions = expand_2d(positions)
-part1 = solve(positions)
 
+part1 = solve(expand_2d(positions, 2))
 puts "Part 1: #{part1}"
+
+part2 = solve(expand_2d(positions, 1_000_000))
+puts "Part 2: #{part2}"
