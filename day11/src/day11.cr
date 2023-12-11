dafce438b43a9867f568208990e349c73ac23315
fwcd <fwcdmail@gmail.com>
2023-12-11 19:55:43 +0000

    Implement expansion on positions alone

--- day11/src/day11.cr
+++ day11/src/day11.cr
@@ -3,32 +3,31 @@ unless ARGV.size >= 1
   exit 1
 end
 
-def expand_vertically(lines)
-  lines.flat_map do |line|
-    if line.includes?('#')
-      [line]
-    else
-      [line, line]
+alias Position = Array(Int32)
+
+def expand_1d(positions : Array(Position), axis : Int32) : Array(Position)
+  sorted = positions.sort_by { |pos| pos[axis] }
+  sorted.reduce({0, [] of Position}) do |acc, pos|
+    offset, acc_positions = acc
+    unless acc_positions.empty?
+      delta = pos[axis] + offset - acc_positions.last[axis]
+      if delta > 1
+        offset += delta - 1
+      end
     end
-  end
+    if offset > 0
+      pos = [*pos]
+      pos[axis] += offset
+    end
+    {offset, [*acc_positions, pos]}
+  end[1]
 end
 
-def transpose(lines)
-  (0...lines[0].size).map do |x|
-    lines.map do |line|
-      line[x]
-    end.join
-  end
+def expand_2d(positions : Array(Position)) : Array(Position)
+  expand_1d(expand_1d(positions, 1), 0)
 end
 
-def expand_2d(lines)
-  lines = expand_vertically(lines)
-  lines = transpose(lines)
-  lines = expand_vertically(lines)
-  transpose(lines)
-end
-
-def find_galaxies(lines)
+def find_galaxies(lines : Array(String)) : Array(Position)
   lines.map_with_index do |line, y|
     line.chars.map_with_index do |c, x|
       if c == '#'
@@ -38,11 +37,11 @@ def find_galaxies(lines)
   end.flat_map &.itself
 end
 
-def manhattan(pos1, pos2)
+def manhattan(pos1 : Position, pos2 : Position) : Int32
   pos1.zip(pos2).map { |ps| (ps[1] - ps[0]).abs }.sum
 end
 
-def solve(positions)
+def solve(positions : Array(Position)) : Int32
   positions.map_with_index do |pos1, i|
     positions[(i + 1)..].map_with_index do |pos2, j|
       manhattan(pos1, pos2)
@@ -53,8 +52,8 @@ end
 path = ARGV[0]
 lines = File.read_lines(path)
 
-lines = expand_2d(lines)
 positions = find_galaxies(lines)
+positions = expand_2d(positions)
 part1 = solve(positions)
 
 puts "Part 1: #{part1}"
