5c2aea4c929c38b04690f99e2545a307dad99ef5
fwcd <fwcdmail@gmail.com>
2023-12-11 18:13:00 +0000

    Parse positions from day 11

--- day11/src/day11.cr
+++ day11/src/day11.cr
@@ -5,4 +5,21 @@ end
 
 path = ARGV[0]
 input = File.read_lines(path)
-puts input
+
+map = input.flat_map do |line|
+  if line.includes?('#')
+    [line]
+  else
+    [line, line]
+  end
+end
+
+positions = map.map_with_index do |line, y|
+  line.chars.map_with_index do |c, x|
+    if c == '#'
+      [x, y]
+    end
+  end.compact
+end.flat_map &.itself
+
+puts positions
