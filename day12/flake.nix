2507746220a583eac452ebe30d8bd87961b48470
fwcd <fwcdmail@gmail.com>
2023-12-12 00:23:57 +0000

    Migrate day 12 to a flake

---
+++ day12/flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "Advent of Code Day 12";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    # Helper functions from https://christine.website/blog/nix-flakes-1-2022-02-21/
+    let forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in {
+      packages = forAllSystems (system:
+        let pkgs = import nixpkgs { inherit system; };
+        in {
+          default = pkgs.stdenv.mkDerivation {
+            name = "advent-of-code-2023-day12";
+            src = ./src;
+
+            buildInputs = [
+              pkgs.luajit
+            ];
+
+            nativeBuildInputs = [
+              pkgs.luajitPackages.moonscript
+            ];
+
+            buildPhase = ''
+              mkdir out
+              moonc -o out/day12.lua day12.moon
+              luajit -bg out/day12.lua out/day12.ljbc
+            '';
+
+            installPhase = ''
+              mkdir -p $out/{bin,share,src}
+              cp day12.moon out/day12.lua $out/src
+              cp out/day12.ljbc $out/share
+
+              cat <<EOF > $out/bin/day12
+              #!/bin/bash
+              exec "${pkgs.luajit.outPath}/bin/luajit" "\$(dirname "\$0")/../share/day12.ljbc" "\$@"
+              EOF
+
+              chmod +x $out/bin/day12
+            '';
+          };
+        }
+      );
+    };
+}
