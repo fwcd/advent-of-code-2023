f2d9a9e9273509f9379cfc1febc57b4b6fbc6499
fwcd <fwcdmail@gmail.com>
2023-12-12 01:18:33 +0000

    Migrate day 5 to a flake

---
+++ day05/flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Advent of Code - Day 05 solution";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in {
+      packages = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in {
+          default = pkgs.callPackage ./derivation.nix {
+            b-compiler = pkgs.callPackage ./b-compiler {};
+          };
+        }
+      );
+
+      apps = forAllSystems (system: {
+        default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/day05";
+        };
+      });
+    };
+}
