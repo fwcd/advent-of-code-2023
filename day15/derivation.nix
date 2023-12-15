f810e119c1ce47f8477a5e820414ea0197d3fa6e
fwcd <fwcdmail@gmail.com>
2023-12-15 14:49:33 +0000

    Add start script for day 15 that passes the input as stdin

--- day15/derivation.nix
+++ day15/derivation.nix
@@ -13,7 +13,19 @@
     '';
 
     installPhase = ''
-      mkdir -p $out/bin
-      cp day15 $out/bin
+      mkdir -p $out/{bin,libexec}
+      cp day15 $out/libexec/day15-impl
+
+      cat <<EOF > $out/bin/day15
+      #!/bin/bash
+      set -e
+      if [ -z "\$1" ]; then
+        echo "Usage: \$(basename "\$0") <path to input>"
+        exit 1
+      fi
+      exec "\$(dirname "\$0")/../libexec/day15-impl" < "\$1"
+      EOF
+
+      chmod +x $out/bin/day15
     '';
   }
