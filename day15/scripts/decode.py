8357aec3a593731fa59e33ee04116e99e1c9c93a
fwcd <fwcdmail@gmail.com>
2023-12-15 17:33:38 +0000

    Use colored characters

--- day15/scripts/decode.py
+++ day15/scripts/decode.py
@@ -2,6 +2,9 @@
 
 import fileinput
 
+RED = '\033[31m'
+CLEAR = '\033[0m'
+
 for bs in fileinput.input(mode='rb'):
     for b in bs:
-        print(f"{chr(b) if b >= 32 and b <= 126 else '?'} {b}")
+        print(f"{chr(b) if b >= 32 and b <= 126 else f'{RED}?{CLEAR}'} {b}")
