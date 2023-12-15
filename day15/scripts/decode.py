f117389c54686e1317ab7c47a8b2cb9264b02a4a
fwcd <fwcdmail@gmail.com>
2023-12-15 17:31:14 +0000

    Output ? for non-decodable characters

--- day15/scripts/decode.py
+++ day15/scripts/decode.py
@@ -4,4 +4,4 @@ import fileinput
 
 for bs in fileinput.input(mode='rb'):
     for b in bs:
-        print(f"{chr(b) if b >= 32 and b <= 126 else ' '} {b}")
+        print(f"{chr(b) if b >= 32 and b <= 126 else '?'} {b}")
