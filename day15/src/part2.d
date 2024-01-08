e3d150677a509cad84f2fa33a50dfa126237debf
fwcd <fwcd@fwcd.dev>
2024-01-08 21:10:17 +0000

    Output part 1 hashes

--- day15/src/part2.d
+++ day15/src/part2.d
@@ -1,16 +1,17 @@
-import std.array, std.conv, std.file, std.stdio;
+import std.array, std.algorithm, std.conv, std.file, std.stdio, std.string;
 
 int hash(string s)
 {
   int value = 0;
   foreach (dchar c; s) {
-    value = ((value + to!int(c)) * 17) % 256;
+    value = ((value + c.to!int) * 17) % 256;
   }
   return value;
 }
 
 void main(string[] args)
 {
-  string[] input = readText(args[1]).split(",");
+  string[] input = readText(args[1]).strip.split(",");
   writefln("Got %(%s,%)", input);
+  writefln("Got %(%d,%)", input.map!(hash));
 }
