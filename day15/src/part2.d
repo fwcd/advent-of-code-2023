3d422d963d5fe01143e7523b18c5d3c7e4c849a3
fwcd <fwcd@fwcd.dev>
2024-01-08 22:12:42 +0000

    Begin implementing part 2

--- day15/src/part2.d
+++ day15/src/part2.d
@@ -1,4 +1,4 @@
-import std.array, std.algorithm, std.conv, std.file, std.stdio, std.string;
+import std.array, std.algorithm, std.conv, std.file, std.stdio, std.string, std.typecons;
 
 int hash(string s)
 {
@@ -12,5 +12,39 @@ int hash(string s)
 void main(string[] args)
 {
   string[] input = readText(args[1]).strip.split(",");
-  writefln("%d", input.map!(hash).sum);
+  Tuple!(string, "key", int, "focalLength")[][256] boxes;
+  int[string] indices;
+
+  foreach (string lens; input) {
+    if (lens[$-1] == '-') {
+      string key = lens[0..$-1];
+      if (key in indices) {
+        int i = indices[key];
+        indices.remove(key);
+        boxes[i].remove!(e => e.key == key);
+        writefln("Box %d after removal of %s: %(%s,%)", i, key, boxes[i].map!(e => e.key));
+      }
+    } else {
+      string[] components = lens.split("=");
+      string key = components[0];
+      int focalLength = components[1].to!int;
+
+      int i = hash(key);
+      int j = boxes[i].countUntil!(e => e.key == key).to!int;
+      if (j < boxes[i].length) {
+        // Lens is already in box, update focal length
+        boxes[i][j].focalLength = focalLength;
+      } else {
+        // Insert lens
+        writefln("Could not find %s in %d, got %d for %s in %(%s,%)", key, i, j, key, boxes[i].map!(e => e.key));
+        boxes[i] ~= tuple!("key", "focalLength")(key, focalLength);
+        writefln("Box %d -> %(%s,%)", i, boxes[i].map!(e => e.key));
+        indices[key] = i;
+      }
+    }
+  }
+
+  // foreach (box; boxes) {
+  //   writefln("%(%s,%) %(%d,%)", box.map!(e => e.key), box.map!(e => e.focalLength));
+  // }
 }
