7ee3bd275dfdf1d2076f0f231008caaa420bfb48
fwcd <fwcd@fwcd.dev>
2024-01-09 01:19:11 +0000

    Stub out day 14 in Go

---
+++ day14/derivation.nix
@@ -0,0 +1,21 @@
+{ stdenv, go }:
+  stdenv.mkDerivation {
+    name = "advent-of-code-2023-day14";
+    src = ./src;
+    sourceRoot = ".";
+
+    nativeBuildInputs = [
+      go
+    ];
+
+    buildPhase = ''
+      mkdir out
+
+      go build -o out/day14 src/day14.go
+    '';
+
+    installPhase = ''
+      mkdir -p $out/bin
+      cp out/day14 $out/bin
+    '';
+  }
