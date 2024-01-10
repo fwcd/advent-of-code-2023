137b18f329f8ccd5c3632b18506659542ab8aec1
fwcd <fwcd@fwcd.dev>
2024-01-10 19:06:51 +0000

    Set up infrastructure for traversing the matrix

--- day16/src/day16.zig
+++ day16/src/day16.zig
@@ -5,6 +5,61 @@ const stdout = std.io.getStdOut().writer();
 
 const String = []u8;
 const Matrix = std.ArrayList(String);
+const Vec2 = struct { x: i32, y: i32 };
+
+fn Set(comptime T: type) type {
+    return std.AutoHashMap(T, void);
+}
+
+fn setsEqual(comptime T: type, a: Set(T), b: Set(T)) bool {
+    var aIter = a.keyIterator();
+    while (aIter.next()) |x| {
+        if (!b.contains(x.*)) {
+            return false;
+        }
+    }
+    var bIter = b.keyIterator();
+    while (bIter.next()) |x| {
+        if (!a.contains(x.*)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+fn step(current: Set(Vec2), matrix: Matrix) Set(Vec2) {
+    _ = matrix;
+    _ = current;
+    var next = Set(Vec2).init(allocator);
+
+    // TODO
+
+    return next;
+}
+
+fn countEnergized(matrix: Matrix) !u32 {
+    var visited = Set(Vec2).init(allocator);
+    defer visited.deinit();
+
+    var current = Set(Vec2).init(allocator);
+    try current.put(.{ .x = 0, .y = 0 }, {});
+
+    var next = Set(Vec2).init(allocator);
+    while (!setsEqual(Vec2, current, next)) {
+        try stdout.print("Looping @ {d}\n", .{current.count()});
+        current.deinit();
+        current = next;
+        next = step(current, matrix);
+        var nextIter = next.keyIterator();
+        while (nextIter.next()) |pos| {
+            try visited.put(pos.*, {});
+        }
+    }
+    next.deinit();
+    current.deinit();
+
+    return visited.count();
+}
 
 pub fn main() !u8 {
     var args = try std.process.argsAlloc(allocator);
@@ -37,7 +92,7 @@ pub fn main() !u8 {
         try stdout.print("{s}\n", .{line});
     }
 
-    try stdout.print("Part 1: {d}\n", .{0});
+    try stdout.print("Part 1: {d}\n", .{try countEnergized(matrix)});
 
     return 0;
 }
