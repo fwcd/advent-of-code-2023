66f29ddf988ecb3e66c0d793a8c915998c7f8508
fwcd <fwcd@fwcd.dev>
2024-01-10 23:37:11 +0000

    Implement Dijkstra

--- day17/src/day17.php
+++ day17/src/day17.php
@@ -5,6 +5,87 @@ if ($argc <= 1) {
   exit(1);
 }
 
-$raw = file_get_contents($argv[1]);
+class Vec2 {
+  public int $x;
+  public int $y;
+
+  function __construct(int $x = 0, int $y = 0) {
+    $this->x = $x;
+    $this->y = $y;
+  }
+
+  function __toString(): string {
+    return "($this->x, $this->y)";
+  }
+
+  function inBounds(int $width, int $height) {
+    return $this->x >= 0 && $this->x < $width && $this->y >= 0 && $this->y < $height;
+  }
+
+  function add(Vec2 $rhs): Vec2 {
+    return new Vec2($this->x + $rhs->x, $this->y + $rhs->y);
+  }
+
+  function turnLeft(): Vec2 {
+    return new Vec2($this->y, -$this->x);
+  }
+
+  function turnRight(): Vec2 {
+    return new Vec2(-$this->y, $this->x);
+  }
+}
+
+class Node {
+  public Vec2 $pos;
+  public Vec2 $dir;
+  public int $total;
+  public int $straightLeft;
+
+  function __construct(Vec2 $pos, Vec2 $dir, int $total, int $straightLeft) {
+    $this->pos = $pos;
+    $this->dir = $dir;
+    $this->total = $total;
+    $this->straightLeft = $straightLeft;
+  }
+}
+
+function shortestPath(array $matrix, int $maxStraight = 3): int {
+  $visited = [];
+  $queue = new \SplPriorityQueue();
+  foreach ([new Vec2(1, 0), new Vec2(0, 1)] as $start) {
+    $queue->insert(new Node($start, $start, 0, $maxStraight - 1), 0);
+  }
+
+  $width = strlen($matrix[0]);
+  $height = count($matrix);
+  $dest = new Vec2($width - 1, $height - 1);
+
+  while ($queue->valid()) {
+    $node = $queue->extract();
+    if ($node->pos == $dest) {
+      return $node->total;
+    }
+    $dirs = [$node->dir->turnLeft(), $node->dir->turnRight()];
+    if ($node->straightLeft > 0) {
+      array_push($dirs, $node->dir);
+    }
+    foreach ($dirs as $dir) {
+      $pos = $node->pos->add($dir);
+      if (!array_key_exists((string) $pos, $visited) && $pos->inBounds($width, $height)) {
+        $visited[(string) $pos] = true;
+        $total = $node->total + intval($matrix[$pos->y][$pos->x]);
+        $straightLeft = (($dir == $node->dir) ? $maxStraight : $node->straightLeft) - 1;
+        $next = new Node($pos, $dir, $total, $straightLeft);
+        $queue->insert($next, -$total);
+      }
+    }
+  }
+
+  return 0;
+}
+
+$raw = trim(file_get_contents($argv[1]));
 $input = preg_split('/\R/', $raw);
-var_dump($input);
+
+$part1 = shortestPath($input);
+echo "Part 1: $part1" . PHP_EOL;
