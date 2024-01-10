8600615cc6b62bf13808743bfa15483df75d5894
fwcd <fwcd@fwcd.dev>
2024-01-10 02:33:36 +0000

    Read input into a Matrix

--- day16/src/day16.zig
+++ day16/src/day16.zig
@@ -3,6 +3,9 @@ const std = @import("std");
 const allocator = std.heap.page_allocator;
 const stdout = std.io.getStdOut().writer();
 
+const String = []u8;
+const Matrix = std.ArrayList(String);
+
 pub fn main() !u8 {
     var args = try std.process.argsAlloc(allocator);
     if (args.len <= 1) {
@@ -10,13 +13,27 @@ pub fn main() !u8 {
         return 1;
     }
 
-    var buf: [1024]u8 = undefined;
+    var matrix = Matrix.init(allocator);
+    defer matrix.deinit();
+
+    var buffer: [1024]u8 = undefined;
     var file = try std.fs.cwd().openFile(args[1], .{});
     var bufReader = std.io.bufferedReader(file.reader());
     var reader = bufReader.reader();
     defer file.close();
 
-    while (try reader.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+    while (try reader.readUntilDelimiterOrEof(&buffer, '\n')) |bufLine| {
+        const line = try allocator.dupeZ(u8, bufLine);
+        try matrix.append(line);
+    }
+
+    defer {
+        for (matrix.items) |line| {
+            allocator.free(line);
+        }
+    }
+
+    for (matrix.items) |line| {
         try stdout.print("{s}\n", .{line});
     }
 
