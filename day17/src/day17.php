700a071d3502f4afda791b33ed24543a928fbc8b
fwcd <fwcd@fwcd.dev>
2024-01-11 00:19:00 +0000

    Remove path tracking

--- day17/src/day17.php
+++ day17/src/day17.php
@@ -52,14 +52,12 @@ class Vec2 {
 class Node {
   public Vec2 $pos;
   public Vec2 $dir;
-  public array $path;
   public int $total;
   public int $straightLeft;
 
-  function __construct(Vec2 $pos, Vec2 $dir, array $path, int $total, int $straightLeft) {
+  function __construct(Vec2 $pos, Vec2 $dir, int $total, int $straightLeft) {
     $this->pos = $pos;
     $this->dir = $dir;
-    $this->path = $path;
     $this->total = $total;
     $this->straightLeft = $straightLeft;
   }
@@ -69,7 +67,7 @@ function shortestPath(array $matrix, int $maxStraight = 3): Node {
   $visited = [];
   $queue = new \SplPriorityQueue();
   foreach ([new Vec2(1, 0), new Vec2(0, 1)] as $start) {
-    $queue->insert(new Node($start, $start, [], intval($matrix[$start->y][$start->x]), $maxStraight - 1), 0);
+    $queue->insert(new Node($start, $start, intval($matrix[$start->y][$start->x]), $maxStraight - 1), 0);
   }
 
   $width = strlen($matrix[0]);
@@ -89,11 +87,10 @@ function shortestPath(array $matrix, int $maxStraight = 3): Node {
     foreach ($dirs as $dir) {
       $pos = $node->pos->add($dir);
       if (!array_key_exists((string) $pos, $visited) && $pos->inBounds($width, $height)) {
-        $path = [...$node->path, $node];
         $cost = intval($matrix[$pos->y][$pos->x]);
         $total = $node->total + $cost;
         $straightLeft = (($dir == $node->dir) ? $node->straightLeft : $maxStraight) - 1;
-        $next = new Node($pos, $dir, $path, $total, $straightLeft);
+        $next = new Node($pos, $dir, $total, $straightLeft);
         $queue->insert($next, -$total);
       }
     }
@@ -107,11 +104,3 @@ $input = preg_split('/\R/', $raw);
 
 $destNode = shortestPath($input);
 echo "Part 1: $destNode->total" . PHP_EOL;
-
-foreach ([...$destNode->path, $destNode] as $node) {
-  $pos = $node->pos;
-  $dir = $node->dir;
-  $input[$pos->y][$pos->x] = $dir->arrow();
-}
-
-echo join(PHP_EOL, $input) . PHP_EOL;
