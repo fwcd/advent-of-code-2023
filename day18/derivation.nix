825922f946f43aeafe6bd32bfde5e8b88c004942
fwcd <fwcd@fwcd.dev>
2024-01-11 12:07:52 +0000

    Set up day 18 in C++

---
+++ day18/derivation.nix
@@ -0,0 +1,16 @@
+{ stdenv, php }:
+  stdenv.mkDerivation {
+    name = "advent-of-code-2023-day18";
+    src = ./src;
+
+    buildPhase = ''
+      mkdir -p out
+      $CXX -o out/day18 day18.cpp
+    '';
+
+    installPhase = ''
+      mkdir -p $out/bin
+      cp out/day18 $out/bin
+    '';
+  }
+
