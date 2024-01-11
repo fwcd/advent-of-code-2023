ffc7df4bf732ad6eb81fc91552d7de58363d9906
fwcd <fwcd@fwcd.dev>
2024-01-11 12:24:00 +0000

    Add day 18 inputs

--- day18/src/day18.cpp
+++ day18/src/day18.cpp
@@ -1,6 +1,16 @@
+#include <fstream>
 #include <iostream>
 
-int main() {
-  std::cout << "Hello world!" << std::endl;
+int main(int argc, char *argv[]) {
+  if (argc == 1) {
+    std::cerr << "Usage: " << argv[0] << " <path to input>" << std::endl;
+    return 1;
+  }
+
+  std::ifstream file;
+  file.open(argv[1]);
+
+  // TODO
+
   return 0;
 }
