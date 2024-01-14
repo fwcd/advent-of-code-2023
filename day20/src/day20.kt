1875aa8f23313ccb7b934425b9566b198c79e084
fwcd <fwcd@fwcd.dev>
2024-01-14 15:14:09 +0000

    Implement file reading in K/Native

--- day20/src/day20.kt
+++ day20/src/day20.kt
@@ -1,3 +1,27 @@
+import kotlinx.cinterop.*
+import platform.posix.*
+
+// https://www.nequalsonelifestyle.com/2020/11/16/kotlin-native-file-io/
+@ExperimentalForeignApi
+fun readLines(filePath: String): List<String> {
+  val lines = mutableListOf<String>()
+  val file = fopen(filePath, "r") ?: throw IllegalArgumentException("Cannot open $filePath")
+  try {
+    memScoped {
+      val bufferLength = 64 * 1024
+      val buffer = allocArray<ByteVar>(bufferLength)
+      var line = fgets(buffer, bufferLength, file)?.toKString()
+      while (line != null) {
+        lines.add(line)
+        line = fgets(buffer, bufferLength, file)?.toKString()
+      }
+    }
+  } finally {
+    fclose(file)
+  }
+  return lines
+}
+
 fun main() {
   print("Hello!")
 }
