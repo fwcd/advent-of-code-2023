785b7b4be24a6ed2f633ea10f6db9b206ff47c58
fwcd <fwcd@fwcd.dev>
2024-01-19 18:35:15 +0000

    Represent maze as integers

--- day21/src/day21.cs
+++ day21/src/day21.cs
@@ -3,31 +3,30 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
-bool IsPopulated(char cell) => cell == 'S' || cell == 'O';
+int Mod(int n, int m) => (n % m + m) % m;
 
-bool InBounds(List<List<char>> maze, int i, int j) =>
-  i >= 0 && i < maze.Count && j >= 0 && j < maze[0].Count;
+bool IsWall(long cell) => cell < 0;
 
-char Get(List<List<char>> maze, int i, int j) =>
-  InBounds(maze, i, j) ? maze[i][j] : ' ';
+long Get(List<List<long>> maze, int i, int j) =>
+  maze[Mod(i, maze.Count)][Mod(j, maze[0].Count)];
 
-IEnumerable<char> Neighbors(List<List<char>> maze, int i, int j) =>
+IEnumerable<long> Neighbors(List<List<long>> maze, int i, int j) =>
   Enumerable.Range(-1, 3)
-    .SelectMany(d => d == 0 ? new List<char>() : new List<char> {
+    .SelectMany(d => d == 0 ? new List<long>() : new List<long> {
       Get(maze, i + d, j),
       Get(maze, i, j + d),
     });
 
-List<List<char>> Step(List<List<char>> maze) =>
+List<List<long>> Step(List<List<long>> maze) =>
   maze
     .Select((row, i) => row
-      .Select((cell, j) => cell == '.' && Neighbors(maze, i, j).Any(IsPopulated)
-        ? 'O'
-        : cell == '#' ? '#' : '.')
+      .Select((cell, j) => IsWall(cell)
+        ? cell
+        : Neighbors(maze, i, j).Where(n => !IsWall(n)).Sum())
       .ToList())
     .ToList();
 
-List<List<char>> StepN(List<List<char>> maze, int n) =>
+List<List<long>> StepN(List<List<long>> maze, int n) =>
   n <= 0 ? maze : StepN(Step(maze), n - 1);
 
 if (args.Length == 0)
@@ -36,16 +35,20 @@ if (args.Length == 0)
   return 1;
 }
 
-List<List<char>> maze = File.ReadAllText(args[0])
+List<List<long>> maze = File.ReadAllText(args[0])
   .Trim()
   .Split('\n')
-  .Select(row => row.ToList())
+  .Select(row => row
+    .Select(cell => cell == 'S' ? 1L : cell == '#' ? -1L : 0L)
+    .ToList())
   .ToList();
 
-int part1 = StepN(maze, 64)
-  .Select(row => row.Count(cell => cell == 'O'))
-  .Sum();
+// long part1 = StepN(maze, 64)
+//   .Select(row => row.Sum())
+//   .Sum();
+// 
+// Console.WriteLine($"Part 1: {part1}");
 
-Console.WriteLine($"Part 1: {part1}");
+Console.WriteLine($"{string.Join("\n", StepN(maze, 2).Select(row => string.Join(", ", row.Select(c => IsWall(c) ? " " : c.ToString()))))}");
 
 return 0;
