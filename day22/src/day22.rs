a389fcdb45442ba5be6eb9e8bb84058c42bc5fbd
fwcd <fwcd@fwcd.dev>
2024-01-20 19:46:09 +0000

    Add Id wrapper for bricks

--- day22/src/day22.rs
+++ day22/src/day22.rs
@@ -2,6 +2,7 @@ use std::{
     convert::{TryInto, TryFrom},
     env,
     fs,
+    ops::{Add, Deref, DerefMut},
     process,
     str::FromStr,
 };
@@ -28,8 +29,16 @@ struct Vec3 {
 }
 
 impl Vec3 {
-    pub fn new(x: i32, y: i32, z: i32) -> Self {
+    fn new(x: i32, y: i32, z: i32) -> Self {
         Self { x, y, z }
+    }
+}
+
+impl Add<Vec3> for Vec3 {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self {
+        Self { x: self.x + rhs.x, y: self.y + rhs.y, z: self.z + rhs.z }
     }
 }
 
@@ -43,9 +52,35 @@ impl FromStr for Vec3 {
 }
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
+struct Id<T> {
+    id: usize,
+    value: T,
+}
+
+impl<T> Deref for Id<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.value
+    }
+}
+
+impl<T> DerefMut for Id<T> {
+    fn deref_mut(&mut self) -> &mut T {
+        &mut self.value
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
 struct Brick {
     start: Vec3,
     end: Vec3,
+}
+
+impl Brick {
+    fn fall(&self) {
+        todo!()
+    }
 }
 
 impl FromStr for Brick {
@@ -59,15 +94,31 @@ impl FromStr for Brick {
 
 #[derive(Debug, PartialEq, Eq, Clone)]
 struct Board {
-    bricks: Vec<Brick>,
+    bricks: Vec<Id<Brick>>,
+}
+
+impl Board {
+    fn collides(&self, brick: &Id<Brick>) -> bool {
+        todo!()
+    }
+
+    fn step() {
+        todo!()
+    }
 }
 
 impl FromStr for Board {
     type Err = String;
 
     fn from_str(raw: &str) -> Result<Self, String> {
-        let bricks = parse_delimited(raw, "\n")?;
-        Ok(Self { bricks })
+        let bricks: Vec<_> = parse_delimited(raw, "\n")?;
+        Ok(Self {
+            bricks: bricks
+                .into_iter()
+                .enumerate()
+                .map(|(id, value)| Id { id, value })
+                .collect()
+        })
     }
 }
 
