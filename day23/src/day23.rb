e2d802ad14c15a28411e56bddb65eb2ba4017816
fwcd <fwcd@fwcd.dev>
2024-01-25 04:24:16 +0100

    Find choice points

--- day23/src/day23.rb
+++ day23/src/day23.rb
@@ -1,9 +1,41 @@
 #!/usr/bin/env ruby
 
+require 'set'
+
+def get(matrix, pos)
+  if pos[0] >= 0 && pos[0] < matrix.size && pos[1] >= 0 && pos[1] < matrix[0].size
+    matrix[pos[0]][pos[1]]
+  end
+end
+
+def neighbors(matrix, pos)
+  [-1, 1]
+    .flat_map { |d| [[d, 0], [0, d]] }
+    .map { |d| [d, pos.zip(d).map { |a| a.inject(:+) }] }
+    .filter do |a|
+      v = get(matrix, a[1])
+      v == '.' ||
+      (a[0] == [ 0,  1] && v == '>') ||
+      (a[0] == [ 0, -1] && v == '<') ||
+      (a[0] == [ 1,  0] && v == 'v') ||
+      (a[0] == [-1,  0] && v == '^')
+    end
+    .map { |a| a[1] }
+end
+
+def longest_path(matrix, visited = Set[], pos = [0, 1])
+  visited.add(pos)
+  while (n = neighbors(matrix, pos).filter { |p| !visited.include?(p) }).size == 1
+    pos = n[0]
+    visited.add(pos)
+  end
+  n
+end
+
 if ARGV.size == 0
   puts "Usage: day23 <path to input>"
   exit 1
 end
 
 matrix = File.readlines(ARGV[0])
-puts matrix
+puts "Part 1: #{longest_path(matrix)}"
