54e0c1f279bcd777390995ff378148c3f1e2a486
fwcd <fwcd@fwcd.dev>
2024-01-25 04:42:56 +0100

    Implement 23.2 naively

--- day23/src/day23.rb
+++ day23/src/day23.rb
@@ -8,34 +8,39 @@ def get(matrix, pos)
   end
 end
 
-def neighbors(matrix, pos)
+def neighbors(matrix, pos, slopes: true)
   [-1, 1]
     .flat_map { |d| [[d, 0], [0, d]] }
     .map { |d| [d, pos.zip(d).map { |a| a.inject(:+) }] }
     .filter do |a|
       v = get(matrix, a[1])
-      v == '.' ||
-      (a[0] == [ 0,  1] && v == '>') ||
-      (a[0] == [ 0, -1] && v == '<') ||
-      (a[0] == [ 1,  0] && v == 'v') ||
-      (a[0] == [-1,  0] && v == '^')
+      v == '.' || (
+        ((!slopes || a[0] == [ 0,  1]) && v == '>') ||
+        ((!slopes || a[0] == [ 0, -1]) && v == '<') ||
+        ((!slopes || a[0] == [ 1,  0]) && v == 'v') ||
+        ((!slopes || a[0] == [-1,  0]) && v == '^')
+      )
     end
     .map { |a| a[1] }
 end
 
-def longest_path(matrix, visited = Set[], pos = [0, 1])
+def longest_path(matrix, visited = Set[], pos = [0, 1], slopes: true)
   visited.add(pos)
 
   # Follow path until we reach a choice point or the end
-  while (n = neighbors(matrix, pos).filter { |p| !visited.include?(p) }).size == 1
+  while (n = neighbors(matrix, pos, slopes: slopes).filter { |p| !visited.include?(p) }).size == 1
     pos = n[0]
     visited.add(pos)
   end
 
   if n.size == 0
-    visited.size - 1
+    if pos == [matrix.size - 1, matrix[0].size - 2]
+      visited.size - 1
+    else
+      0
+    end
   else
-    n.map { |p| longest_path(matrix, visited.clone, p) }.max
+    n.map { |p| longest_path(matrix, visited.clone, p, slopes: slopes) }.max
   end
 end
 
@@ -45,4 +50,8 @@ if ARGV.size == 0
 end
 
 matrix = File.readlines(ARGV[0])
-puts "Part 1: #{longest_path(matrix)}"
+  .map { |l| l.strip }
+  .filter { |l| !l.empty? }
+
+puts "Part 1: #{longest_path(matrix, slopes: true)}"
+puts "Part 2: #{longest_path(matrix, slopes: false)}"
