4b46304164a302a2bf04771cb5415fe1e2b2e7cf
fwcd <fwcd@fwcd.dev>
2024-02-06 21:34:44 +0100

    Read day 24 input

--- day24/src/Day24.hs
+++ day24/src/Day24.hs
@@ -1,2 +1,14 @@
+import System.Environment (getArgs)
+import System.Exit (exitFailure)
+import System.IO (readFile')
+
 main :: IO ()
-main = putStrLn "Hello world!"
+main = do
+  args <- getArgs
+  case args of
+    [path] -> do
+      raw <- readFile' path
+      putStrLn raw
+    _ -> do
+      putStrLn "Usage: day24 <path to input>"
+      exitFailure
